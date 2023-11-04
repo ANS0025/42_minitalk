3922344efcf82a05ae97df5a88131b5fc9626640
ANS0025 <niko.aegis@gmail.com>
2023-11-05 01:56:12 +0900

    add: finished project v1

--- server.c
+++ server.c
@@ -1,55 +1,46 @@
 #include "minitalk.h"
 
-void bin_to_char(int signum, char* c)
+void	stoc(int sig, siginfo_t *info, void *context)
 {
-    static int i = 0;
-    if (signum == SIGUSR1)
-        *c = (*c << 1) | 1;
-    else if (signum == SIGUSR2)
-        *c <<= 1;
-    if (++i == 8)
-    {
-        i = 0;
-        if (!*c)
-        {
-            kill(getpid(), SIGUSR1);
-            *c = '\0';
-        }
-        printf("%c", *c);
-        *c = '\0';
-    }
+	static int	bit;
+	static int	word;
+
+	(void)context;
+	if (sig == SIGUSR1)
+		word += 1 << bit;
+	bit++;
+	if (bit == 8)
+	{
+		write (1, &word, 1);
+		if (word == '\0')
+		{
+			kill(info->si_pid, SIGUSR2);
+		}
+		word = 0;
+		bit = 0;
+	}
 }
 
-void sig_handler(int signum, siginfo_t* info, void* context)
+int	main(int argc, char **argv)
 {
-    static int pid = 0;
-    static int i = 0;
-    static char c = '\0';
-    (void)context;
-    pid = info->si_pid;
-    bin_to_char(signum, &c);
-    if (++i == 32)
-    {
-        i = 0;
-        kill(pid, SIGUSR1);
-    }
-    else
-        kill(pid, SIGUSR2);
-}
+	int					pid;
+	struct sigaction	act;
 
-int main(void)
-{
-    struct sigaction sa;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_SIGINFO;
-    sa.sa_sigaction = sig_handler;
-    printf("Server PID: %d\n", getpid());
-    if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
-    {
-        printf("Error setting up signal handlers\n");
-        return (1);
-    }
-    while (1)
-        pause();
-    return (0);
+	(void)argv;
+	if (argc != 1)
+	{
+		printf("ERROR, No arguments needed!\n");
+		return (1);
+	}
+	ft_printf("PID : ");
+	pid = getpid();
+	ft_printf("%d\n", pid);
+	act.sa_sigaction = stoc;
+	act.sa_flags = 0;
+	while (argc == 1)
+	{
+		sigaction(SIGUSR1, &act, NULL);
+		sigaction(SIGUSR2, &act, NULL);
+	}
+	return (0);
 }
